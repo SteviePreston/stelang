7bd277eb13f20240aae877be26ae9d8711808526
steviep <stevenpreston2014@gmail.com>
2025-04-22 23:04:18 +0100

    feat: Added next token method and a switch statement for parsing in the lexer

--- interpreter/lexer/lexer.go
+++ interpreter/lexer/lexer.go
@@ -1,6 +1,8 @@
 package lexer
 
-import "github/steviepreston/stelang/interpeter/token"
+import (
+	token "github.com/steviepreston/stelang/interpreter/token"
+)
 
 type Lexer struct {
 	input        string
@@ -23,4 +25,36 @@ func (l *Lexer) readChar() {
 	}
 	l.postiton = l.readPostiton
 	l.readPostiton += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
