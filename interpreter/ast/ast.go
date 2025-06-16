f7bd7af00c4802bd4966ffe2ded9323c07eb4ae2
steviep <stevenpreston2014@gmail.com>
2025-06-16 21:53:04 +0100

    feat: parse funcs

--- interpreter/ast/ast.go
+++ interpreter/ast/ast.go
@@ -15,6 +15,12 @@ type Statement interface {
 	statementNode()
 }
 
+type PrefixExpression struct {
+	Token    token.Token
+	Operator string
+	Right    Expression
+}
+
 type Expression interface {
 	Node
 	expressionNode()
@@ -118,3 +124,16 @@ func (i *Identifer) String() string       { return i.Value }
 func (il *IntegerLiteral) expressionNode()      {}
 func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 func (il *IntegerLiteral) String() string       { return il.Token.Literal }
+
+func (pe *PrefixExpression) expressionNode()      {}
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
--- interpreter/parser/parser.go
+++ interpreter/parser/parser.go
@@ -9,7 +9,7 @@ import (
 )
 
 const (
-	_ int = itoa
+	_ int = iota
 	LOWEST
 	EQUALS
 	LESSGREATER
@@ -47,7 +47,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifer)
 	p.registerPrefix(token.INT, p.parseIntegerLiteral)
-
+	p.registerPrefix(token.BANG, p.parseIntegerLiteral)
+	p.registerPrefix(token.MINUS, p.parseIntegerLiteral)
 	return p
 }
 
@@ -112,9 +113,25 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	return stmt
 }
 
+func (p *Parser) parsePrefixExpression() ast.Expression {
+	expression := &ast.PrefixExpression{
+		Token:    p.curToken,
+		Operator: p.curToken.Literal,
+	}
+	p.nextToken()
+	expression.Right = p.parseExpression(PREFIX)
+	return expression
+}
+
+func (p *Parser) noPrefixParseFnError(t token.TokenType) {
+	msg := fmt.Sprintf("no prefic parse function for %s found", t)
+	p.errors = append(p.errors, msg)
+}
+
 func (p *Parser) parseExpression(precedence int) ast.Expression {
 	prefix := p.prefixParseFns[p.curToken.Type]
 	if prefix == nil {
+		p.noPrefixParseFnError(p.curToken.Type)
 		return nil
 	}
 	leftExp := prefix()
