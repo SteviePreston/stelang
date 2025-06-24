48d11e9179c3893d6a9b2fd96dbb4b75a6fcae1b
StevieP <78793772+SteviePreston@users.noreply.github.com>
2025-06-24 23:09:13 +0100

    Finished Parsing (#3)
    * chore: Updated readme.md and getting ready for the next chapter of parsing tokens
    
    * feat: Added AST and some Parsing Logic
    
    * feat: Added return statement parsing
    
    * feat: Added ast logic for strings
    
    * feat: parser and ast work parsing integer literals
    
    * feat: parse funcs
    
    * feat: parse funcs and tests
    
    * feat: parse funcs, tests and tracing
    
    * feat: Added parsing logic around if statements and blocks
    
    * feat: Finished Pratt Parser for now...

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # stelang
+
+## NOTE: This is not production ready, this is just a learning exercise for myself...
+
+#### This is inspired by the Thorsten Ball books. I will put the base stages as branches...
+
+I will be building ontop of these in the future building my understanding of how code actually works
+Plus I have always wanted to create my own language :p
--- interpreter/lexer/lexer.go
+++ interpreter/lexer/lexer.go
@@ -94,10 +94,6 @@ func (l *Lexer) NextToken() token.Token {
 	return tok
 }
 
-func newToken(tokenType token.TokenType, ch byte) token.Token {
-	return token.Token{Type: tokenType, Literal: string(ch)}
-}
-
 func (l *Lexer) readIdentifier() string {
 	position := l.position
 	for isLetter(l.ch) {
@@ -134,34 +130,6 @@ func (l *Lexer) skipWhitespace() {
 	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
 		l.readChar()
 	}
-}
-
-func (l *Lexer) NextToken() token.Token {
-	var tok token.Token
-
-	switch l.ch {
-	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
-	case ';':
-		tok = newToken(token.SEMICOLON, l.ch)
-	case '(':
-		tok = newToken(token.LPAREN, l.ch)
-	case ')':
-		tok = newToken(token.RPAREN, l.ch)
-	case ',':
-		tok = newToken(token.COMMA, l.ch)
-	case '+':
-		tok = newToken(token.PLUS, l.ch)
-	case '{':
-		tok = newToken(token.LBRACE, l.ch)
-	case '}':
-		tok = newToken(token.RBRACE, l.ch)
-	case 0:
-		tok.Literal = ""
-		tok.Type = token.EOF
-	}
-	l.readChar()
-	return tok
 }
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
